dc95d1186abae5c396bb60390b8b996da720f82a
smdissanayake <129976854+smdissanayake@users.noreply.github.com>
2025-05-21 21:21:42 +0530

    Update web.php
    yasthi's update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\PatientController;
 use App\Http\Controllers\Api\DrugController;
@@ -45,5 +47,33 @@ Route::get('/patient-notes', [PatientController::class, 'getPatientNotes']);
 Route::get('/order-summary', function () {
     return inertia('OrderSummary');
 
+});
+
+Route::get('/patients/search-suggestions', function (Request $request) {
+    $query = $request->get('query');
+    
+    if (empty($query)) {
+        return response()->json([]);
+    }
+
+    $patients = DB::table('patients')
+        ->where('clinicRefNo', 'like', '%' . $query . '%')
+        ->select('id', 'clinicRefNo', 'firstName', 'lastName')
+        ->orderByRaw('CASE 
+            WHEN clinicRefNo = ? THEN 1
+            WHEN clinicRefNo LIKE ? THEN 2
+            ELSE 3
+        END', [$query, $query . '%'])
+        ->limit(5)
+        ->get()
+        ->map(function ($patient) {
+            return [
+                'id' => $patient->id,
+                'clinicRefNo' => $patient->clinicRefNo,
+                'name' => $patient->firstName . ' ' . $patient->lastName
+            ];
+        });
+
+    return response()->json($patients);
 });
 
