3b5ef8ba41b5acd9abf502c0876aace0285e8adc
smdissanayake <129976854+smdissanayake@users.noreply.github.com>
2025-05-21 21:26:33 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\PatientController;
+use App\Http\Controllers\Api\DrugController;
+use App\Http\Controllers\Api\PrescriptionController;
 
 Route::get('/', function () {
     return inertia('Home');
@@ -23,9 +26,6 @@ Route::get('/test-data', function () {
     ]);
 });
 
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\PatientController;
-
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
 
@@ -33,36 +33,16 @@ Route::post('/patients', [PatientController::class, 'store']);
 Route::get('/patients/search-by-clinic-ref', [PatientController::class, 'findByClinicRefNo']);
 Route::post('/patient-history-examination', [PatientController::class, 'storeHistoryExamination']);
 Route::get('/patient-history-examination/{patient_id}', [PatientController::class, 'getHistoryExaminationRecords']);
+
+Route::get('/drugs', [DrugController::class, 'index']);
+Route::post('/drugs', [DrugController::class, 'store']);
+
+// Routes for Prescriptions and Special Items
+Route::post('/prescriptions', [PrescriptionController::class, 'store']);
+
 Route::post('/patient-notes', [PatientController::class, 'storeNote']);
 Route::get('/patient-notes', [PatientController::class, 'getPatientNotes']);
 Route::get('/order-summary', function () {
     return inertia('OrderSummary');
-});
 
-Route::get('/patients/search-suggestions', function (Request $request) {
-    $query = $request->get('query');
-    
-    if (empty($query)) {
-        return response()->json([]);
-    }
-
-    $patients = DB::table('patients')
-        ->where('clinicRefNo', 'like', '%' . $query . '%')
-        ->select('id', 'clinicRefNo', 'firstName', 'lastName')
-        ->orderByRaw('CASE 
-            WHEN clinicRefNo = ? THEN 1
-            WHEN clinicRefNo LIKE ? THEN 2
-            ELSE 3
-        END', [$query, $query . '%'])
-        ->limit(5)
-        ->get()
-        ->map(function ($patient) {
-            return [
-                'id' => $patient->id,
-                'clinicRefNo' => $patient->clinicRefNo,
-                'name' => $patient->firstName . ' ' . $patient->lastName
-            ];
-        });
-
-    return response()->json($patients);
 });
