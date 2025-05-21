09ecf265275c6360b33eb861b26b7046bcd10157
shehan <shehandissanayaka45@gmail.com>
2025-05-21 21:44:52 +0530

    up web

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,32 @@ Route::get('/medical-orders/patient/{patientId}', [MedicalOrderController::class
 Route::get('/medical-orders/clinic-ref', [OrderController::class, 'getOrdersByClinicRefNo']);
 Route::get('/medical-orders/{id}', [OrderController::class, 'show']);
 Route::delete('/medical-orders/{id}', [MedicalOrderController::class, 'destroy']);
+
+});
+Route::get('/patients/search-suggestions', function (Request $request) {
+    $query = $request->get('query');
+    
+    if (empty($query)) {
+        return response()->json([]);
+    }
+
+    $patients = DB::table('patients')
+        ->where('clinicRefNo', 'like', '%' . $query . '%')
+        ->select('id', 'clinicRefNo', 'firstName', 'lastName')
+        ->orderByRaw('CASE 
+            WHEN clinicRefNo = ? THEN 1
+            WHEN clinicRefNo LIKE ? THEN 2
+            ELSE 3
+        END', [$query, $query . '%'])
+        ->limit(5)
+        ->get()
+        ->map(function ($patient) {
+            return [
+                'id' => $patient->id,
+                'clinicRefNo' => $patient->clinicRefNo,
+                'name' => $patient->firstName . ' ' . $patient->lastName
+            ];
+        });
+
+    return response()->json($patients);
+});
